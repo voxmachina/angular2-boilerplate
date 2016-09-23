93d65795165daee3691d825b87e2a26efb0cc8fb
Pedro Eugenio <pedro.t.eugenio@gmail.com>
2016-09-23 12:26:22 +0200

    Component splitup

--- app/components/app/app.module.ts
+++ app/components/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule }      from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { MediumPostsComponent } from "../medium-posts/medium-posts.component";
+
+import { MediumService } from "../../services/medium.service";
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    MediumPostsComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [MediumService]
 })
 
 export class AppModule { }
--- app/services/medium.service.ts
+++ app/services/medium.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from "@angular/core";
+import { MediumPost } from "../models/medium-post";
+
+export const MediumPosts: MediumPost[] = [
+  {id: 1, title: "1", content: "content 1"},
+  {id: 2, title: "2", content: "content 2"},
+  {id: 3, title: "3", content: "content 3"}
+];
 
 @Injectable()
 export class MediumService {
-  getPosts(): void { }
+  getPosts(): Promise<MediumPost[]> {
+    return Promise.resolve(MediumPosts);
+  }
 }
