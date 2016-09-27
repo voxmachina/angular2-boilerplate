0a866a5944c7212ab9d56dac6212e88ed23e3e89
Pedro Eugenio <pedro.t.eugenio@gmail.com>
2016-09-27 10:19:35 +0200

    Added medium api call

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-igeni.us personal website
+# igeni.us personal website
+
+gulp dev -- development task with watchers
+gulp deploy -- be sure to add your ssh configuration to config/gulp.json before
+
+npm start to start the frontend server
+
+sh api/www/services/start to start the apache-php Docker instance
--- api/www/services/content/app/Http/routes.php
+++ api/www/services/content/app/Http/routes.php
@@ -11,10 +11,41 @@
 |
 */
 
-$app->get('/', function () use ($app) {
-    return $app->version();
-});
-
+/**
+* Returns the latest medium feed in json format
+* Uses 2 hours file cache mechanism
+*/
 $app->get('medium', function () use ($app) {
-    return response()->json(['name' => 'dummy', 'state' => 'bar']);
+
+  $cacheFile = "../storage/medium.json";
+  $exists = file_exists($cacheFile);
+
+  if( !$exists || ( $exists && time() > strtotime('+2 hours', filemtime($cacheFile)))) {
+    $url = 'https://medium.com/@peugenio/latest?format=json';
+
+    $curl = curl_init();
+    curl_setopt_array(
+      $curl,
+      [
+        CURLOPT_URL            => $url,
+        CURLOPT_USERAGENT      => 'spider',
+        CURLOPT_TIMEOUT        => 120,
+        CURLOPT_CONNECTTIMEOUT => 30,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_ENCODING       => 'UTF-8'
+      ]
+    );
+    $data = curl_exec($curl);
+    curl_close($curl);
+
+    // Clean medium protection on json feed
+    $data = explode("</x>", $data);
+    $data = $data[1];
+
+    file_put_contents($cacheFile, $data);
+  } else {
+    $data = file_get_contents($cacheFile);
+  }
+
+  return response()->json(json_decode($data));
 });
