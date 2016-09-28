977c73fc6c95d110c616a97adb2f8cd6664b9a71
Pedro Eugenio <pedro.t.eugenio@gmail.com>
2016-09-28 16:26:32 +0200

    Removed unused dependencies

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core";
-import { MediumPost } from "../../models/medium-post";
-import { MediumService } from "../../services/medium.service";
 
 @Component({
   selector: "dashboard",
