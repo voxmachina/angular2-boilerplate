862d1a99ceafcdc01210720d2bd41bc17bf46245
Pedro Eugenio <pedro.t.eugenio@gmail.com>
2016-09-29 09:47:28 +0200

    improved deploy process

--- gulpfile.js
+++ gulpfile.js
@@ -311,12 +311,39 @@ gulp.task('symlink', ['compress'], function() {
     ]);
 });
 
+/**
+ * Cleans release files
+ */
+gulp.task('clean-release', function(cb) {
+  return del([
+    'release/app/components',
+    'release/app/models',
+    'release/app/services',
+    'release/app/config.js',
+    'release/config',
+    'release/lib/shim.min.js',
+    'release/lib/zone.js',
+    'release/lib/Reflect.js',
+    'release/lib/system.src.js',
+    'release/lib/core.umd.js',
+    'release/lib/common.umd.js',
+    'release/lib/compiler.umd.js',
+    'release/lib/platform-browser.umd.js',
+    'release/lib/platform-browser-dynamic.umd.js',
+    'release/lib/http.umd.js',
+    'release/lib/router.umd.js',
+    'release/lib/forms.umd.js',
+    'release/lib/rxjs'
+  ],
+  cb);
+});
+
 gulp.task('stage', function(done) {
-  runSequence('build', 'release', 'minify-html-css', 'inline', 'bundle', 'uglify', 'clean-helpers');
+  runSequence('build', 'release', 'minify-html-css', 'inline', 'bundle', 'uglify', 'clean-helpers', 'clean-release');
   done();
 });
 
 gulp.task('deploy', function(done) {
-  runSequence('build', 'release', 'minify-html-css', 'inline', 'bundle', 'uglify', 'clean-helpers', 'symlink', 'clean');
+  runSequence('build', 'release', 'minify-html-css', 'inline', 'bundle', 'uglify', 'clean-helpers', 'clean-release', 'symlink', 'clean');
   done();
 });
